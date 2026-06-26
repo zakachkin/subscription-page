52edc106fb3d85dc56b1e20dc6205fd092a06c08
zakachkin <viktordemo@gmail.com>
2026-06-27 00:04:51 +0300

    fix: do not require cookies for static assets

--- backend/src/common/middlewares/check-assets-cookie.middleware.ts
+++ backend/src/common/middlewares/check-assets-cookie.middleware.ts
@@ -1,45 +1,5 @@
 import { NextFunction, Request, Response } from 'express';
-import * as jwt from 'jsonwebtoken';
 
-import { Logger } from '@nestjs/common';
-
-import { IJwtPayload } from '@common/constants';
-
-const logger = new Logger('CheckAssetsCookieMiddleware');
-
-export function checkAssetsCookieMiddleware(
-    req: { user: IJwtPayload } & Request,
-    res: Response,
-    next: NextFunction,
-) {
-    if (req.path.startsWith('/assets') || req.path.startsWith('/locales')) {
-        const secret = process.env.INTERNAL_JWT_SECRET;
-
-        if (!secret) {
-            logger.error('INTERNAL_JWT_SECRET is not set');
-            res.socket?.destroy();
-
-            return;
-        }
-
-        if (!req.cookies.session) {
-            logger.debug('No session cookie found');
-            res.socket?.destroy();
-
-            return;
-        }
-
-        try {
-            const jwtPayload = jwt.verify(req.cookies.session, secret);
-
-            req.user = jwtPayload as unknown as IJwtPayload;
-        } catch (error) {
-            logger.debug(error);
-            res.socket?.destroy();
-
-            return;
-        }
-    }
-
+export function checkAssetsCookieMiddleware(_req: Request, _res: Response, next: NextFunction) {
     return next();
 }
